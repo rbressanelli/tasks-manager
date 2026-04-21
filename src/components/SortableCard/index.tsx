c0a4c432e8825fe28190baae5b071a380866acc0
Roberto Bressanelli <rbressanelli@gmail.com>
2026-04-21 14:20:34 -0300

    Refactor: changed card border

--- src/components/SortableCard/index.tsx
+++ src/components/SortableCard/index.tsx
@@ -17,7 +17,7 @@ export function SortableCard({ id, index, group, modal, onDelete, data }: {
   });
 
   return (
-    <div ref={ref} className="border-2 border-[hsl(231,87%,41%)] cursor-pointer p-[10px] w-full min-h-[100px] bg-white shadow-[2px_2px_10px_rgba(0,0,0,0.6)] transition-all duration-200 ease-in-out relative grid grid-cols-[4fr_1fr] grid-rows-[4fr_1fr]"
+    <div ref={ref} className="border-1 cursor-pointer p-[10px] w-full min-h-[100px] bg-white shadow-[2px_2px_10px_rgba(0,0,0,0.6)] transition-all duration-200 ease-in-out relative grid grid-cols-[4fr_1fr] grid-rows-[4fr_1fr]"
     >
       <p
         onClick={(e) => {
